c98a61911bda7be7c67e164c122c8180969b20e0
edan184 <edan184@hotmail.com>
2022-06-18 19:34:30 -0700

    first commit

---
+++ Kiwiscraper.py
@@ -0,0 +1,44 @@
+import requests, pymysql
+from bs4 import BeautifulSoup
+
+list_examp = ["Yogi Berra", "Thomas Edison"]
+
+# gets data from typefit api
+def collect_quotes_data(quote_page):
+
+    return
+
+def parse_quote_data():
+
+    return
+
+def create_img_data():
+
+    return
+
+def create_quote_data():
+
+    return
+
+# save all this data in database somehow
+def update_db(img_set, quote_set):
+    result = requests.get("https://en.wikipedia.org/wiki/{}".input(name))
+
+# function that takes an author 
+# makes a request to https://en.wikipedia.org/wiki/Carl_Sandburg
+# finds all image srcs using bs4 
+def db_query(name):
+
+
+# parses data that returns
+# hashmap key: author, value: array of quotes
+
+# function that takes an author
+# makes a request to https://en.wikipedia.org/wiki/Carl_Sandburg
+# finds all image srcs using bs4 
+
+# call functions 1 and 2
+# iterate of key *authors list from hashmap 
+# call function 3 
+# save srcs 
+# hashmap (author -> list of image srcs)
